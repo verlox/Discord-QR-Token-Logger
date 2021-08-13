3cb89aa1f0b6cfe05cae75ecac40b8dd51079702
verlox <verlox@riseup.net>
2021-08-13 15:55:25 -0400

    update colors + new info panel

---
+++ Discord-QR-Token-Stealer/info.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Discord_QR_Token_Stealer
+{
+    public partial class info : Form
+    {
+        public info()
+        {
+            InitializeComponent();
+        }
+
+        void openUrl(string url)
+        {
+            var proc = new Process();
+            var info = new ProcessStartInfo();
+            info.FileName = url;
+            proc.StartInfo = info;
+            proc.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            openUrl("https://verlox.cc");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            openUrl("https://github.com/verlox");
+        }
+    }
+}
